82231437fe8adb0d47285e15f2fbc5a9b3760b35
Yaser Kasim <YaserKasim@outlook.com>
2022-08-12 13:58:23 +0100

    Test final update

--- app/index.test.js
+++ app/index.test.js
@@ -2,9 +2,10 @@ import stuff from "./index.js";
 
 describe("tryConvert", () => {
   it("should convert farentheit to celcuis", () => {
-    expect({(string|number)});
+    expect(string|number)});
     {expect(getCelcius(celcuis) == fahrenheit);
-    if Boolean expect(getCelcius(celcuis) == fahrenheit);
+    if (getCelcius(celcuis) == fahrenheit) ;
+    else { throw exception ; "conversion failed"}
     }
   });
-});
+})
