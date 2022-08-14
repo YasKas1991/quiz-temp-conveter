c1e0a168f941d3f141ffb6b7090221e1e04059d4
Yaser Kasim <YaserKasim@outlook.com>
2022-08-14 20:45:39 +0100

    Add with error

--- app/features/step_definitions/steps.mjs
+++ app/features/step_definitions/steps.mjs
@@ -1,3 +1,14 @@
-import { Given } from "@cucumber/cucumber";
+import { Given, When, Then } from "@cucumber/cucumber";
+import tryConvert from "../../app/index.js";
 
-Given("I have a temperature of {float}° {units}", (float, units) => {});
+Given("I have a temperature of {float}° {units}", (temp2Convert, _) => {
+  this.temp2Convert = temp2Convert;
+});
+
+When("I convert it to degrees {units}", (units) => {
+  this.convertedTemp = tryConvert(this.temp2Convert, units);
+});
+
+Then("I see {float}° {untis}", (expected, _) => {
+  expect(this.covertedTemp).toBe(expected.toString());
+});
