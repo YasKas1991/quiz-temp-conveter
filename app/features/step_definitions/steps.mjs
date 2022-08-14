ac6347d24998aeea5783a9af4ea1e594da1310e6
Yaser Kasim <YaserKasim@outlook.com>
2022-08-14 21:18:46 +0100

    Add 3 tests passes

--- app/features/step_definitions/steps.mjs
+++ app/features/step_definitions/steps.mjs
@@ -9,6 +9,18 @@ When("I convert it to degrees {units}", (units) => {
   this.convertedTemp = tryConvert(this.temp2Convert, units);
 });
 
-Then("I see {float}° {untis}", (expected, _) => {
-  expect(this.covertedTemp).toBe(expected.toString());
+Then("I see {float}° {units}", function (expected, _) {
+  const expectHelper = () => {
+    expect(this.convertedTemp).toBe(expected.toString());
+  };
+
+  expectHelper();
+});
+
+Then(" I see a blank string", function () {
+  const expectHelper = () => {
+    expect(this.convertedTemp).toBe("");
+  };
+
+  expectHelper();
 });
