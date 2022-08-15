cd9d9573932a31fd8f61726bd04e85d970a5334b
Yaser Kasim <YaserKasim@outlook.com>
2022-08-15 14:48:40 +0100

    Add additional comments

--- app/features/step_definitions/steps.mjs
+++ app/features/step_definitions/steps.mjs
@@ -1,16 +1,22 @@
 import { Given, When, Then } from "@cucumber/cucumber";
 import tryConvert from "../../app/index.js";
 
+// Arrange
 Given("I have a temperature of {float}° {units}", (temp2Convert, _) => {
+  console.log(this);
   this.temp2Convert = temp2Convert;
 });
 
+// Act
 When("I convert it to degrees {units}", (units) => {
+  console.log(this);
   this.convertedTemp = tryConvert(this.temp2Convert, units);
 });
 
+// Assert
 Then("I see {float}° {units}", function (expected, _) {
   const expectHelper = () => {
+    console.log(this);
     expect(this.convertedTemp).toBe(expected.toString());
   };
 
@@ -19,6 +25,7 @@ Then("I see {float}° {units}", function (expected, _) {
 
 Then(" I see a blank string", function () {
   const expectHelper = () => {
+    console.log(this);
     expect(this.convertedTemp).toBe("");
   };
 
---
+++ app/throwaway.js
@@ -0,0 +1,26 @@
+const person = {
+  name: "John",
+  age: 30,
+  greet() {
+    console.log(this);
+    console.log(`Hi, I'm ${this.name}`);
+  },
+  greet2: () => {
+    console.log(this);
+    console.log(`Hi, I'm ${this.name}`);
+  },
+  greet3: function () {
+    console.log(this);
+    console.log(`Hi, I'm ${this.name}`);
+  },
+
+  // TODO: Use a nested object with nested methods
+  greet4: {
+    msg: "Good morning",
+    sayHello() {
+      console.log(this);
+      console.log(`${this.msg} ${this.name}`);
+    },
+    saySomethingElse: () => {
+      console.log(this);
+      console.log(`${this.msg} ${this.name}`);
